79c7823574ad7d8055b1df70c339f5a74f130bd5
vickykohad <vickykohad26@gmail.com>
2024-05-14 21:26:19 +0530

    new updates

---
+++ src/test/java/easymytrip/example/easymytrip/EasymytripApplicationTests.java
@@ -0,0 +1,13 @@
+package easymytrip.example.easymytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
