18790e1afdc4aea405e79adc18474bd73210a691
vickykohad <vickykohad26@gmail.com>
2024-11-09 20:33:12 +0530

    hello

---
+++ src/main/java/easymytrip/example/easymytrip/bus.java
@@ -0,0 +1,19 @@
+package easymytrip.example.easymytrip;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class bus {
+
+    @GetMapping("/bus")
+
+    public String getData() {
+        return "please book your bus ticket on vande bharat 50% discount" ; }
+
+}
+
+
